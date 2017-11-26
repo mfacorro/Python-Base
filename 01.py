13d62992799b311899a18aa6d2c908e39f001198
Martin Facorro <martin.dev@openmailbox.org>
2017-11-25 22:11:13 -0300

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+#+--------------------------------------------------+
+#|  Practicas Python                                |
+#|       Archivo: 01.py                             |
+#|      Creacion: Martin Facorro 25 Nov 2017        |
+#|        Editor: VI                                |
+#+--------------------------------------------------+
+
